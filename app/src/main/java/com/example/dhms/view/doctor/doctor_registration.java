5ee6056c05c962cda0181bb0766a91109971d42b
Surya Bharathi VK <suryafrizzera@gmail.com>
2021-03-13 22:02:39 +0530

    2 commit

---
+++ app/src/main/java/com/example/dhms/view/doctor/doctor_registration.java
@@ -0,0 +1,16 @@
+package com.example.dhms.view.doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dhms.R;
+
+public class doctor_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_registration);
+    }
+}
