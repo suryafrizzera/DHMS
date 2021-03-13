7b745e70a0fdff1489ae6cb644b4a9fe4d9dfe77
Surya Bharathi VK <suryafrizzera@gmail.com>
2021-03-13 22:02:01 +0530

    2 commit

--- app/src/main/java/com/example/dhms/view/doctor/doctor_signup.java
+++ app/src/main/java/com/example/dhms/view/doctor/doctor_signup.java
@@ -3,19 +3,41 @@ package com.example.dhms.view.doctor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.dhms.R;
 
 public class doctor_signup extends AppCompatActivity {
 
-    EditText username,password,confirm_password;
+    EditText username,email,password,confirm_password;
     Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_signup);
+        username=findViewById(R.id.editTextusername);
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        confirm_password = findViewById(R.id.editTextconfirmTextPassword);
+        signup = findViewById(R.id.register_button);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!TextUtils.equals(password.getText().toString().trim(),confirm_password.getText().toString().trim())){
+                    Toast.makeText(doctor_signup.this, "Enter the same password in confirm password",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    String mail = email.getText().toString().trim();
+                    String password_text = confirm_password.getText().toString().trim();
+                    String username_text = username.getText().toString().trim();
+
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dhms/view/patient/patient_login.java
+++ app/src/main/java/com/example/dhms/view/patient/patient_login.java
@@ -2,15 +2,27 @@ package com.example.dhms.view.patient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.dhms.R;
 
 public class patient_login extends AppCompatActivity {
 
+    TextView signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_login);
+        signup=findViewById(R.id.signup_link);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(patient_login.this,patient_signup.class));
+                            }
+        });
     }
 }
