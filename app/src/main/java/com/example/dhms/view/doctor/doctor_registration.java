c6e7cf872be02928c82023d758ac5bcbf5f312bd
Surya Bharathi VK <suryafrizzera@gmail.com>
2021-03-14 12:00:52 +0530

    3 commit

--- app/src/main/java/com/example/dhms/view/doctor/doctor_registration.java
+++ app/src/main/java/com/example/dhms/view/doctor/doctor_registration.java
@@ -3,14 +3,77 @@ package com.example.dhms.view.doctor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.dhms.R;
 
-public class doctor_registration extends AppCompatActivity {
+import java.util.HashMap;
 
+public class doctor_registration extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] genders = {"Male","Female"};
+    Spinner sp;
+    String gender="None";
+    EditText first_name,last_name,hospital,specialization,phone,mlicense,qualification;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_registration);
+        sp = findViewById(R.id.spinner_gender);
+        first_name = findViewById(R.id.editTextTextPersonName);
+        last_name = findViewById(R.id.editTextTextPersonName2);
+        hospital = findViewById(R.id.hospital_edittext);
+        specialization = findViewById(R.id.specialization_edittext);
+        phone = findViewById(R.id.phone_edittext);
+        mlicense = findViewById(R.id.M_License_edittext);
+        qualification = findViewById(R.id.qualification_edittext);
+        submit = findViewById(R.id.submit_button);
+
+
+        sp.setOnItemSelectedListener(this);
+        ArrayAdapter ad = new ArrayAdapter(this,android.R.layout.simple_spinner_item,genders);
+        ad.setDropDownViewResource(
+                android.R.layout
+                        .simple_spinner_dropdown_item);
+        sp.setAdapter(ad);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!TextUtils.isEmpty(first_name.getText().toString()) &&
+                        !TextUtils.isEmpty(last_name.getText().toString()) &&
+                        !TextUtils.isEmpty(phone.getText().toString()) &&
+                        !TextUtils.isEmpty(hospital.getText().toString()) &&
+                        !TextUtils.isEmpty(specialization.getText().toString()) &&
+                        !TextUtils.isEmpty(mlicense.getText().toString()) &&
+                        !TextUtils.isEmpty(qualification.getText().toString()) &&
+                        !TextUtils.equals(gender,"None")
+                ){
+                    HashMap<String,Object> regdetails = new HashMap<>();
+                }
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        gender = genders[position];
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/example/dhms/view/doctor/doctor_signup.java
+++ app/src/main/java/com/example/dhms/view/doctor/doctor_signup.java
@@ -1,25 +1,47 @@
 package com.example.dhms.view.doctor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.dhms.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class doctor_signup extends AppCompatActivity {
 
+    FirebaseAuth auth ;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference doctor = db.collection("doctor");
     EditText username,email,password,confirm_password;
     Button signup;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_signup);
+
+        auth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
         username=findViewById(R.id.editTextusername);
         email = findViewById(R.id.editTextTextEmailAddress);
         password = findViewById(R.id.editTextTextPassword);
@@ -32,10 +54,48 @@ public class doctor_signup extends AppCompatActivity {
                     Toast.makeText(doctor_signup.this, "Enter the same password in confirm password",Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    String mail = email.getText().toString().trim();
-                    String password_text = confirm_password.getText().toString().trim();
-                    String username_text = username.getText().toString().trim();
 
+                    signup.setVisibility(View.INVISIBLE);
+                    progressBar.setVisibility(View.VISIBLE);
+                    final String mail = email.getText().toString().trim();
+                    String password_text = confirm_password.getText().toString().trim();
+                    final String username_text = username.getText().toString().trim();
+                    auth.createUserWithEmailAndPassword(mail,password_text)
+                  .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                      @Override
+                      public void onSuccess(AuthResult authResult) {
+                          Toast.makeText(doctor_signup.this,"Registered Successfully",Toast.LENGTH_SHORT)
+                          .show();
+                          progressBar.setVisibility(View.INVISIBLE);
+                          signup.setVisibility(View.VISIBLE);
+                          HashMap<String,Object> user = new HashMap<>();
+                          user.put("username",username_text);
+                          user.put("email",mail);
+                          String mydate = java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+                          user.put("date created",mydate);
+                          doctor.add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                              @Override
+                              public void onSuccess(DocumentReference documentReference) {
+                                  startActivity(new Intent(doctor_signup.this,doctor_registration.class));
+
+                              }
+                          }).addOnFailureListener(new OnFailureListener() {
+                              @Override
+                              public void onFailure(@NonNull Exception e) {
+                                  Toast.makeText(doctor_signup.this,e.getMessage() + "Try again",Toast.LENGTH_SHORT)
+                                          .show();
+                              }
+                          });
+                      }
+                  }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            signup.setVisibility(View.VISIBLE);
+                            Toast.makeText(doctor_signup.this,e.getMessage(),Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    });
                 }
             }
         });
